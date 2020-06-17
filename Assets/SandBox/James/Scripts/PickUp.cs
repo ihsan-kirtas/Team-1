f8b8ff0646289202f2a47568254a483f5b8bc85c
James Siebert <js@media360.com.au>
2020-06-18 09:11:22 +1000

    Updated James SandBox scene
    Experimenting with a simple raycast item pickup  script and added a patient arrival zone and 4 bed positions into my scene so i can test scripts.

---
+++ Assets/SandBox/James/Scripts/PickUp.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    // The distance the player has to be from the item for the pickup to work.
+    public int distanceToItem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    void OnGUI()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Collect();
+        
+    }
+
+    private void Collect()
+    {
+        // only activate Ray casting on mouse up - efficiency boost.
+        if (Input.GetMouseButton(1)) // 0 = left click, 1 = right click
+        {
+            RaycastHit hit;
+            
+            // Ray firing from mouse position.
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            // cast ray our at defined distance to item.
+            if (Physics.Raycast(ray, out hit, distanceToItem))
+            {
+                // If the ray hits item
+                if (hit.collider.gameObject.name == "item")
+                {
+                    Debug.Log("item hit");
+                    
+                    // Destroy item
+                    Destroy(hit.collider.gameObject);
+                }
+            }
+        }
+    }
+}
