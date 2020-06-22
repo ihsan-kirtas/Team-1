d0582ee0eb100943de4dc42a4d9983e22f80a799
James Siebert <js@media360.com.au>
2020-06-22 12:17:28 +1000

    Created ER & Main Menu Scenes
    Created the start menu scene and the main ER scene. Nothing really in them yet, just adding them so we can start working in there.
    + I did some pro builder testing in my sandbox

---
+++ Assets/Scripts/StartMenuController.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Temp bypass until start menu is ready
+        SceneManager.LoadScene("Emergency Room");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Emergency Room");
+    }
+}
