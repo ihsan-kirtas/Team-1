e2ffbd822e0843db2ccfe7f8adad37cb4d9b9cdd
Anne Caves <1014251@student.sae.edu.au>
2020-07-06 15:27:12 +1000

    UI Folder Added to Assets AI pathway script created

---
+++ Assets/Scripts/AC_AnimationController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AC_AnimationController : MonoBehaviour
+{
+    [SerializeField] Animator myAnimatorController;
+    public GameObject NPC;
+
+    private void OnTriggerEnter(Collider other)
+    {   
+        if (other.CompareTag("Assistant 3 NPC"))
+        {
+            myAnimatorController.SetBool("IdleTrue", true);
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Assistant 3 NPC"))
+        {
+            myAnimatorController.SetBool("IdleTrue", false);
+        }
+
+    }
+}
