f21899e65fe3dbdcc5b6b6c9e68503e3a938978b
Jacqueline H <1015612@student.sae.edu.au>
2020-07-20 15:39:25 +1000

    Created script for Settings Menu - Not tested in build
    I have created the script for the options menu to include a resolution dropdown, a fullscreen toggle and a volume slider. I have scripted the functionality for the dropdown and toggle and created the basis for the volume slider which is commented out with a debug statement in place currently.

---
+++ Assets/Scripts/JH_SettingsMenuUI.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class JH_SettingsMenuUI : MonoBehaviour
+{
+    //public AudioMixer audioMixer;                           //Audio hasn't been set up yet but the scripting for the functionality is set to be implemented - this hasn't been tested
+
+    Resolution[] resolutions;
+
+    public Dropdown resolutionDropdown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetVolume(float volume)
+    {
+        Debug.Log(volume);
+        //audioMixer.SetFloat("volume", volume);              //The name in the brackets in the name of the exposed parameter in the Audio Mixer
+    }
+
+    public void SetFullScreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+        Debug.Log("Fullscreen toggle test");
+    }
+}
