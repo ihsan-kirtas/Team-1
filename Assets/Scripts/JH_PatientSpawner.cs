dfc328bfd01e8bca48c2e0d2159619e072f273f6
Jacqueline H <1015612@student.sae.edu.au>
2020-07-20 17:10:14 +1000

    Started on possible Patient Spawner Script and issues with animations
    I started to work on a script to get the patients to spawn in the scene using tags on the spawn points. I still need to figure out how to populate the patients array and how to instantiate them. I also tried to work on animations to be used for background patients but I had issues getting the mixamo animations to work.

---
+++ Assets/Scripts/JH_PatientSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JH_PatientSpawner : MonoBehaviour
+{
+    public GameObject[] spawnPoints;
+    public GameObject[] patients;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            spawnPoints[i] = GameObject.FindGameObjectsWithTag("SpawnPoints")[i]; //Find tag in the dropdown menu on an item prefab
+        }
+
+        //for (int i = 0; i < patients.Length; i++)
+        //{
+        //    patients[i] = GameObject.FindGameObjectsWithTag("Patients")[i]; // can't do this because patients don't exist in game yet
+        //}
+
+        //Instantiate(patients[], spawnPoints[].transform.position, Quaternion.identity); //Do Not know how to finish this but I am working on this as a way to spawn in our patients from an array
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
