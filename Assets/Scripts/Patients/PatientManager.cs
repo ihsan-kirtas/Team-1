9c0a68c5a171b8335d716b88c52d5fe8935e3736
James Siebert <js@media360.com.au>
2020-07-23 09:00:22 +1000

    Starting Patient Manager Scripts + cleanup

---
+++ Assets/Scripts/Patients/PatientManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatientManager : MonoBehaviour
+{
+    // Lists all the possible patients in the game
+    public List<GameObject> allPatients;
+
+    // Lists all of the patients that currently exist in the game (spawned in)
+    public List<GameObject> activePatients;
+}
