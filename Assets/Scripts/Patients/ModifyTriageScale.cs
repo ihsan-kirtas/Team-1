1532ae1415ad9fc888b1e84bd69ec58e0073f77d
Anne Caves <1014251@student.sae.edu.au>
2020-07-28 19:29:35 +1000

    Triage trigger script modified

--- Assets/Scripts/Patients/ModifyTriageScale.cs
+++ Assets/Scripts/Patients/ModifyTriageScale.cs
@@ -9,31 +9,23 @@ public class ModifyTriageScale : MonoBehaviour
     // for buttons
     
     public Patient_Data currentPatientData;
-    
+    public GameObject resusBed;
+    public GameObject bedspaceBed;
     public GameObject triageCanvas;//triage canvas 
+    public Transform resusTarget;
+    public Transform bedspaceTarget;
+    private NavMeshAgent agent;
 
 
 
 
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
         triageCanvas.SetActive(false);//set all objects as false so that the player can triage using a key
     }
 
-    public void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            triageCanvas.SetActive(true);
-            Debug.Log("triage key pressed");
-        }
-
-        else if (Input.GetKeyDown(KeyCode.N))
-        {
-            triageCanvas.SetActive(false);
-        }
-    }
+    
 
 
     private void OnTriggerEnter(Collider player)
@@ -49,6 +41,8 @@ public class ModifyTriageScale : MonoBehaviour
         {
             triageCanvas.SetActive(false);
         }
+
+        // on triage trigger enter triage scale will be enabled player will select correct triage score of 1 or 2
     }
 
     void SetTriageScale(int cat)
@@ -65,32 +59,36 @@ public class ModifyTriageScale : MonoBehaviour
     void setScale1()
     {
         SetTriageScale(1);
-        
+        resusBed.SetActive(false);
+        agent.SetDestination(resusTarget.position);// resus bay 1 is the target position 
     }
 
     void setScale2()
     {
         SetTriageScale(2);
-        
+        resusBed.SetActive(false);
+        agent.SetDestination(resusTarget.position); // resus bay 1 is the target position 
     }
 
     void setScale3()
     {
         SetTriageScale(3);
-        
+        bedspaceBed.SetActive(false);
+        agent.SetDestination(bedspaceTarget.position);// bedspace 1 is the target position 
     }
 
     void setScale4()
     {
         SetTriageScale(4);
-        
-
+        bedspaceBed.SetActive(false);
+        agent.SetDestination(bedspaceTarget.position);// bedspace 1 is the target position 
     }
 
     void setScale5()
     {
         SetTriageScale(5);
-       
+        bedspaceBed.SetActive(false);
+        agent.SetDestination(bedspaceTarget.position);// bedspace 1 is the target position 
     }
 }
 
