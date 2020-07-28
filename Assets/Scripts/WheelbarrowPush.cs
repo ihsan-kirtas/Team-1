14fb69b4be03c9edeb3fe11a8c2ba76edaddd254
Harlem E <1017058@student.sae.edu.au>
2020-07-28 19:53:26 +1000

    Animation Bed Push

---
+++ Assets/Scripts/WheelbarrowPush.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WheelbarrowPush : MonoBehaviour
+{
+    public Transform target;
+
+    private NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //alllow script to comm with navmesh agent
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //use navmesh to move agent towards target
+        agent.SetDestination(target.position);
+    }
+}
