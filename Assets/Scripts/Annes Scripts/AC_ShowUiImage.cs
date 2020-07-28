40d1aca6d0f488d5496f827209722ed689a2a86c
Anne Caves <1014251@student.sae.edu.au>
2020-07-28 11:23:41 +1000

    Background Audio Added

---
+++ Assets/Scripts/Annes Scripts/AC_ShowUiImage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AC_ShowUiImage : MonoBehaviour
+{
+    public GameObject GregPatientJourney;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GregPatientJourney.SetActive(false);
+        StartCoroutine(PatientJourney());
+    }
+
+    IEnumerator PatientJourney()
+    {
+        yield return new WaitForSeconds (180);
+        GregPatientJourney.SetActive(true);
+        yield return new WaitForSeconds(15);
+        GregPatientJourney.SetActive(false);
+    }
+}
