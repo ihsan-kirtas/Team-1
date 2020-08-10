53bf308df981125c2e5c8c395cf8ecd2d27ff557
James Siebert <js@media360.com.au>
2020-08-11 09:31:01 +1000

    Added sound and particle trigger to fountain

---
+++ Assets/Scripts/ModelScripts/WaterFountain.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterFountain : MonoBehaviour
+{
+    private AudioSource audioData;
+
+    void Start()
+    {
+        // Dont play particles
+        GetComponent<ParticleSystem>().Stop();
+
+        // Find Audio
+        audioData = GetComponent<AudioSource>();
+
+        // Start playing sopund clip
+        audioData.Play(0);
+
+        // Pause sound clip
+        audioData.Pause();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            // Animation
+            GetComponent<Animation>().Play("start animation");
+
+            // Find and play the particles
+            GetComponent<ParticleSystem>().Play();
+
+            // Un-pause the audio clip
+            audioData.UnPause();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            // Animation
+            GetComponent<Animation>().Play("end animation");
+
+            // stop the particles
+            GetComponent<ParticleSystem>().Stop();
+
+            // pause the sound
+            audioData.Pause();
+        }
+    }
+}
