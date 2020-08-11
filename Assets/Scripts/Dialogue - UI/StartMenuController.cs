9fca2d3ca7bb20504abfa44dab6207dab4951255
Anne Caves <1014251@student.sae.edu.au>
2020-08-11 23:24:39 +1000

    Peter Wilbourne data added

--- Assets/Scripts/Dialogue - UI/StartMenuController.cs
+++ Assets/Scripts/Dialogue - UI/StartMenuController.cs
@@ -1,4 +1,9 @@
+<<<<<<< Updated upstream
 using System.Collections;
+=======
+<<<<<<< Updated upstream
+﻿using System.Collections;
+>>>>>>> Stashed changes
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -83,4 +88,76 @@ public class StartMenuController : MonoBehaviour
         yield return new WaitForSeconds(1);
         loadingScreen.SetActive(false);
     }
+<<<<<<< Updated upstream
 }
+=======
+}
+=======
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartMenuController : MonoBehaviour
+{
+    public GameObject mainMenuUI;                           //This is called MainMenuUIButtons in the editor
+    public GameObject mainMenuSettingMenuUI;                //This currently does not exist and has a stand in
+    public GameObject creditPageStandIn;                    //This is currently a stand in and may not be used like this in the final build
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Main Scene");
+    }
+
+    public void StartTutorial()
+    {
+        SceneManager.LoadScene("TutorialScene");
+    }
+    
+    public void SettingMenuUI()
+    {
+        mainMenuSettingMenuUI.SetActive(true);
+        mainMenuUI.SetActive(false);
+    }
+
+    public void ReturnToMainMenu()
+    {
+        mainMenuUI.SetActive(true);
+        mainMenuUI.GetComponent<Animator>().enabled = false;
+        mainMenuSettingMenuUI.SetActive(false);
+    }
+
+    public void Credits()
+    {
+        //SceneManager.LoadScene("___");                      //This is the most probable way this will work and the functionality is coded - for now the UI element is a stand in to shown functionality.
+        creditPageStandIn.SetActive(true);
+        mainMenuUI.SetActive(false);
+    }
+
+    public void CreditsReturnToMainMenu()
+    {
+        mainMenuUI.SetActive(true);
+        mainMenuUI.GetComponent<Animator>().enabled = false;
+        creditPageStandIn.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Quiet Application");
+        Application.Quit();
+    }
+}
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
