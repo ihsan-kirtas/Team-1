49fefd6e45f7a2f6b37997d35ff8e243ab650496
Anne Caves <1014251@student.sae.edu.au>
2020-08-11 12:50:48 +1000

    triage score UI data script edited

--- Assets/Scripts/PatientInformation.cs
+++ Assets/Scripts/PatientInformation.cs
@@ -12,6 +12,12 @@ public class PatientInformation : MonoBehaviour
     public Text preexistingmedcond;
     public Text initalobs;
     public Text ambulancebayhandover;
+    public Text currentObs;
+    public Text ample;
+    public Text recommendedDecision;
+    public Text patientJourney;
+    public Text clinicalReasoning;
+
 
 
 
@@ -32,7 +38,15 @@ public class PatientInformation : MonoBehaviour
             patientname.text = patient_data.name;
             patientname.text = patient_data.uiambohandover;
             patientage.text = patient_data.age.ToString();
-            ambulancebayhandover.text = patient_data.ambulanceBayConversation.Last().ToString();
+            ambulancebayhandover.text = patient_data.uiambohandover;
+            currentObs.text = patient_data.currentObs;
+            ample.text = patient_data.ampleText;
+            recommendedDecision.text = patient_data.recommendations;
+            patientJourney.text = patient_data.patienthospitaljourney;
+            clinicalReasoning.text = patient_data.clinicalReferences;
+
+
+            //ambulancebayhandover.text = patient_data.ambulanceBayConversation.Last().ToString();
 
             
         } else
