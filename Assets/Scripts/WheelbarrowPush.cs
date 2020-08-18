62436b0b50fa764deca48f1fe439004f41dd7701
Harlem E <1017058@student.sae.edu.au>
2020-08-18 15:33:27 +1000

    Animation on Pushing Nurse and Bed edited

--- Assets/Scripts/WheelbarrowPush.cs
+++ Assets/Scripts/WheelbarrowPush.cs
@@ -7,18 +7,33 @@ public class WheelbarrowPush : MonoBehaviour
 {
     public Transform target;
 
+    private Animator animator;
+
     private NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         //alllow script to comm with navmesh agent
         agent = GetComponent<NavMeshAgent>();
+        //use navmesh to move agent towards target
+        agent.SetDestination(target.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //use navmesh to move agent towards target
-        agent.SetDestination(target.position);
+
+        Vector2 charPos = new Vector2(transform.position.x, transform.position.z);
+        Vector2 tarPos = new Vector2(agent.destination.x, agent.destination.z);
+
+        if (Vector2.Distance(charPos, tarPos) < 0.2)
+        {
+            animator.SetBool("Walking", false);
+        }
+        else
+        {
+            animator.SetBool("Walking", true);
+        }
     }
 }
