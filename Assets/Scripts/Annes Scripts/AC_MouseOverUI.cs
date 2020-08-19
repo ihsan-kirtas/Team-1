41035c5d540287ca7c72101106d0db42ad1688d8
Anne Caves <1014251@student.sae.edu.au>
2020-08-20 01:11:25 +1000

    MouseOverUi description canvas created

---
+++ Assets/Scripts/Annes Scripts/AC_MouseOverUI.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AC_MouseOverUI : MonoBehaviour
+{
+    public string myString;
+    public Text myText;
+    public float fadeTime;
+    public bool displayInfo;
+
+    private void Start()
+    {
+        myText = GameObject.Find("Text").GetComponent<Text>();
+        myText.color = Color.clear;
+    }
+
+    public void Update()
+    {
+        FadeText();
+
+        //if (Input.GetKeyDown(KeyCode.Escape))
+        //{
+        //    Screen.lockCursor = false;
+        //}
+    }
+
+    private void OnMouseOver()
+    {
+        displayInfo = true;
+    }
+
+    private void OnMouseExit()
+    {
+        displayInfo = false;
+    }
+
+    void FadeText()
+    {
+        if (displayInfo)
+        {
+            myText.text = myString;
+            myText.color = Color.Lerp(myText.color, Color.white, fadeTime * Time.deltaTime);
+        }
+
+        else
+        {
+            myText.color=Color.Lerp(myText.color, Color.clear, fadeTime * Time.deltaTime);
+        }
+    }
+}
