80573884dc8e7d980f41bda3c8caa43cf6e8fdc1
Anne Caves <1014251@student.sae.edu.au>
2020-08-24 23:15:12 +1000

    Raycasting, emergency scene edited audio added new patient data added

--- Assets/Scripts/Annes Scripts/AC_RaycastScript.cs
+++ Assets/Scripts/Annes Scripts/AC_RaycastScript.cs
@@ -5,87 +5,37 @@ using UnityEngine.UI;
 
 public class AC_RaycastScript : MonoBehaviour
 {
-    public float range = 50f;
+    //[SerializeField] private Animator myAnimationController;
     public Camera fpsCam;
-    public float fadeTime;
-    //public GameObject uiCanvas;
-    public Text myText;
-    public string objectName;
-    public Ray ray;
-    public  RaycastHit hit;
+    public float range = 5f;
 
-    public bool displayInfo;
-
-
-    private void Start()
-
-    {
-        myText = GameObject.Find("Text").GetComponent<Text>();
-        myText.color = Color.clear;
-    }
-
+    
 
     private void Update()
     {
-       
-
-        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit))
+        if (Input.GetButtonDown("Fire1"))
         {
-            displayInfo = true;
-            FadeText();
+            OpenCloseDoor();
         }
-
     }
 
-    void FadeText()
+    void OpenCloseDoor()
     {
-        if (displayInfo)
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
-            myText.text = objectName;
-            myText.color = Color.Lerp(myText.color, Color.white, fadeTime * Time.deltaTime);
-            Debug.Log("change object name");
+            PlayAnimation();
+            Debug.Log("door handle hit");
         }
 
-        else
-        {
-            myText.color = Color.Lerp(myText.color, Color.clear, fadeTime * Time.deltaTime);
-        }
+
     }
 
-
-
-
-
-
-
-    //public Camera playerCam;
-    //public Ray ray;
-    ////public float range = 2.0f;
-
-    //private void Update()
-    //{
-    //    if (Input.GetButtonDown("Fire1"))
-    //    {   
-
-    //        //(RaycastHit.collider.gameObject)
-    //        IdentifyObject();
-    //    }
-
-    //    RaycastHit hit;
-    //    if (Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit))
-    //    {
-    //        Debug.Log(hit.transform.name);
-    //    }
-    //}
-
-    //void IdentifyObject()
-    //{
-    //    RaycastHit hit;
-    //    if (Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit))
-    //    {
-    //        Debug.Log(hit.transform.name);
-    //    }
-    //}
+    void PlayAnimation()
+    {
+        //myAnimationController.SetBool("PlayDoorOpen", true);
+        GetComponent<Animation>().Play();
+        Debug.Log("animation plays");
+    }
 }
 
