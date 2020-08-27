654c3327d8cea6c720e6032cd2ce3cd788a68bda
Ihsan Kirtas <ottoman9696@gmail.com>
2020-08-27 13:12:28 +1000

    Main menu animation now only plays once

---
+++ Assets/Scripts/MainAnimationCintroller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainAnimationCintroller : MonoBehaviour
+{
+    public static bool mainstart=true ;
+    public GameObject title;
+    public GameObject mainAnimation;
+    public Animator animator;
+
+    IEnumerator Start()
+    {
+        //animator = GetComponent<Animator>();
+        if (mainstart == true)
+        yield return new WaitForSeconds(9);
+        mainstart = false;
+        title.SetActive(true);
+        mainAnimation.SetActive(false);
+        animator.SetBool("QuickView", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+
+    }
+
+}
