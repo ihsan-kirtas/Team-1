7cea2cd8fa4afad6fc4b228cba0a2bdff215af61
Ihsan Kirtas <ottoman9696@gmail.com>
2020-08-28 09:22:53 +1000

    Fixed Cursor on animations

---
+++ Assets/Scripts/Animation/Cursorvisible.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursorvisible : MonoBehaviour
+{
+    public bool cursorvisibility;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cursorvisibility == true)
+        {
+            Cursor.visible = true;
+        }
+        
+        if (cursorvisibility == false)
+        {
+            Cursor.visible = false;
+        }
+    }
+}
