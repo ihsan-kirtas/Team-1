aef0e6479bec9863f323cfc529113c806f6c35c7
Jacqueline H <1015612@student.sae.edu.au>
2020-08-27 12:05:17 +1000

    Pause Menu working in Emergency Dept scene
    I got the pause menu working and connected up

--- Assets/Scripts/Annes Scripts/AC_PauseMenuED.cs
+++ Assets/Scripts/Annes Scripts/AC_PauseMenuED.cs
@@ -8,19 +8,15 @@ public class AC_PauseMenuED : MonoBehaviour
     public GameObject pauseMenuUIPanel;             // This is for the panel that is used to darken the screen when the game is paused. The Pause Menu UI is under this.
     public GameObject pauseMenuUI;                  // This is the Pause Menu UI which is under the panel - connect this to PauseMenuUIBackground in Unity
     public GameObject settingsMenuUI;               // Connect this to SettingMenuUIBackground in Unity
-    public GameObject terminologyPageUI;
+    public GameObject terminologyPageUI;            // Connect this to TerminologyPageBackground in Unity
     public static bool GameIsPaused = false;
    
-    
-
 
     private void Start()
     {
-        
-        pauseMenuUI.SetActive(false);
+        pauseMenuUIPanel.SetActive(false);
     }
-
-
+    
     // Update is called once per frame
     void Update()
     {
@@ -37,29 +33,31 @@ public class AC_PauseMenuED : MonoBehaviour
         }
     }
 
-   public void Resume()
+    public void Resume()
     {
-        pauseMenuUI.SetActive(false);
-        
-        Time.timeScale = 1f;
+        pauseMenuUIPanel.SetActive(false);          // Turns off the Pause Menu Panel
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        Time.timeScale = 1f;                        // Resumes the game at normal speed
         GameIsPaused = false;
     }
 
-   public void Pause()
+    void Pause()
     {
-        pauseMenuUI.SetActive(true);
-        
-        Time.timeScale = 0f;
+        pauseMenuUIPanel.SetActive(true);           // Turns on the Pause Menu Panel
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        Time.timeScale = 0f;                        // Pauses the game
         GameIsPaused = true;
     }
 
-   
-
-
     public void RestartFreeRoam()
     {
-        pauseMenuUI.SetActive(false);          // Turns off the Pause Menu Panel
-        GameEvents.current.CheckCameraLock();       // Checks wheather to Lock / Unlock Camera
+        pauseMenuUIPanel.SetActive(false);          // Turns off the Pause Menu Panel
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
 
         Time.timeScale = 1f;                        // Resumes the game at normal speed
         GameIsPaused = false;
@@ -67,9 +65,37 @@ public class AC_PauseMenuED : MonoBehaviour
         SceneManager.LoadScene("Emergency Dept");    //Using scene names as Index hasn't been worked on yet. This will need to be changed in the script - I forgot how to make it changable in Unity
     }
 
+    public void SettingsMenu()                      // SWITCH PAGES
+    {
+        pauseMenuUI.SetActive(false);               // Turn OFF Pause BG panel
+        settingsMenuUI.SetActive(true);             // Turn ON Settings BG panel
+    }
+
+    public void SettingsReturnToPauseMenu()         // SWITCH PAGES
+    {
+        settingsMenuUI.SetActive(false);            // Turn OFF Settings BG panel
+        pauseMenuUI.SetActive(true);                // Turn ON Pause BG panel
+    }
+
+    public void TerminologyPage()                   // SWITCH PAGES
+    {
+        pauseMenuUI.SetActive(false);               // Turn OFF Pause BG panel
+        terminologyPageUI.SetActive(true);          // Turn ON Terminology BG panel
+    }
+
+    public void TerminologyReturnToPauseMenu()      // SWITCH PAGES
+    {
+        terminologyPageUI.SetActive(false);         // Turn OFF Terminology BG panel
+        pauseMenuUI.SetActive(true);                // Turn ON Pause BG panel
+    }
+
     public void ExitToMenu()
     {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
         Time.timeScale = 1f;                        // Resumes the game at normal speed
+
         SceneManager.LoadScene("Start Menu");       // Using scene name as no Index exist currently
     }
 
@@ -77,24 +103,6 @@ public class AC_PauseMenuED : MonoBehaviour
     {
         Application.Quit();                         // Quits the application
     }
-
-    //public void ShowSettingPanel()
-    //{
-
-    //    if (GameIsPaused)
-    //    {
-    //        settingsUI.SetActive(false);
-    //        Time.timeScale = 0f;
-    //    }
-    //    else
-    //    {
-    //        settingsUI.SetActive(true);
-    //        Time.timeScale = 0f;
-    //    }
-
-    //}
-
-
 }
 
 
