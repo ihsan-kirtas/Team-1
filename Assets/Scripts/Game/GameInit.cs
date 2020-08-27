3c01bb2307e867dacc872805d0254b77ba54a132
James Siebert <js@media360.com.au>
2020-08-27 19:17:38 +1000

    added game init script

---
+++ Assets/Scripts/Game/GameInit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Check to make sure cursor lock / ui is all good
+        GameEvents.current.CheckCameraLock();
+    }
+
+
+}
